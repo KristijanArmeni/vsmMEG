76ef7b864c09d5326bcaef25ee6efe96d0e5791a
Kristijan Armeni <kriarm@mentat004.dccn.nl>
2018-05-24 09:20:28 +0200

    Added option for ploting differences if two models are provided

--- meg/vsm_plot_accuracy.m
+++ meg/vsm_plot_accuracy.m
@@ -1,16 +1,21 @@
-function stat = vsm_plot_accuracy(model)
+function stat = vsm_plot_accuracy(model1, model2)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 ft_hastoolbox('cellfunction', 1);
+rho = cell(numel(model1.trial), 1);
 
-rho = cell(numel(model.trial), 1);
-
-for k=1:numel(model.trial)
-    rho{k} = model.weights{k}.pearson; %  say model based on 'audio_avg only'}
+for k=1:numel(model1.trial)
+    
+    if nargin > 1 
+        rho{k} = model1.weights{k}.pearson - model2.weights{k}.pearson; %  say model based on 'audio_avg only'}
+    else
+        rho{k} = model1.weights{k}.pearson;
+    end
+    
 end
 
-% average trial specific r
+% average trial specific r (differences)
 rhoavg = cellmean(rho', 2);
 stat   = rhoavg;
 
@@ -44,7 +49,16 @@ view([90, 10]);
 l = camlight;
 material dull;
 h = colorbar;
-ylabel(h, 'corr. coef.');
+if nargin > 1
+    ylabel(h, 'delta r');
+else
+    ylabel(h, 'r');
+end
+if nargin > 1
+    title('Model1 - model2'); 
+else
+    title('Prediction performance');
+end
 set(h, 'Position', [0.8 0.6 0.02 0.15])
 
 end
